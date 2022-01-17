4fd38842b26ee36f8995f9d574235dc899711de7
Olga <olgalopezg@outlook.cl>
2022-01-17 11:15:46 -0300

    Primer commit incorpora animal.js

---
+++ assets/js/animal.js
@@ -0,0 +1,54 @@
+export default class Animal{
+    #nombre
+    #edad
+    #imagen
+    #comentarios
+    #sonido
+    constructor(nombre, edad,imagen,comentarios,sonido){
+        this.#nombre = nombre
+        this.#edad = edad
+        this.#imagen = imagen
+        this.#comentarios = comentarios
+        this.#sonido= sonido
+    }
+
+    get nombre() {
+        return this.#nombre
+    }
+
+    get edad() {
+        return this.#edad
+    }
+
+    get imagen() {
+        return this.#imagen
+    }
+
+    get comentarios() {
+        return this.#comentarios
+    }
+
+    get sonido() {
+        return this.#sonido
+    }
+}
+
+export default class Leon extends Animal{
+    rugir() {}
+}
+
+export default class Lobo extends Animal{
+    aullar(){}
+}
+
+export default class Oso extends Animal{
+    grunir(){}
+}
+
+export default class Serpiente extends Animal{
+    sisear(){}
+}
+
+export default class Aguila extends Animal{
+    chillar(){}
+}
---
+++ assets/js/index.js
@@ -0,0 +1,3 @@
+// Estructura de datos
+//https://developer.mozilla.org/es/docs/Learn/HTML/Multimedia_and_embedding/Video_and_audio_content
+
